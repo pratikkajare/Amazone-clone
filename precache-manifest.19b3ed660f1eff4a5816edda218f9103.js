3fa9212252cf5aedfcf4ed3a2b9e9c282b42aa3a
pratik826 <75242181+pratik826@users.noreply.github.com>
2022-05-19 12:00:23 +0530

    Updates

--- precache-manifest.19b3ed660f1eff4a5816edda218f9103.js
+++ precache-manifest.19b3ed660f1eff4a5816edda218f9103.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "4bd45a3a19c594da5778",
+    "revision": "08ac0b7e072f60f77966",
     "url": "/Amazone-clone/static/css/main.d2d49a61.chunk.css"
   },
   {
-    "revision": "4bd45a3a19c594da5778",
-    "url": "/Amazone-clone/static/js/main.4bd45a3a.chunk.js"
+    "revision": "08ac0b7e072f60f77966",
+    "url": "/Amazone-clone/static/js/main.08ac0b7e.chunk.js"
   },
   {
     "revision": "c589853b1b38597cde06",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/Amazone-clone/static/js/runtime~main.32a60f80.js"
   },
   {
-    "revision": "fb1412d8a9828157b8db4755127d1df7",
+    "revision": "de9fd71698217f2f8e3f4088306c3b75",
     "url": "/Amazone-clone/index.html"
   }
 ];
